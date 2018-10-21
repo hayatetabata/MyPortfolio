4b3a820126ea3d3a20494f093c3af1aa26e71e00
田畑颯 <chinarra.tabata@gmail.com>
2018-10-21 23:20:12 +0900

    Create about index

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,5 +1,39 @@
-<template>
-  <div>
-    About
-  </div>
+<template lang="pug">
+  div(id="container")
+    div(id="header")
+      a(href="/") トップへ
+      h1 プロフィール
+    div(class="profile")
+      img(src="https://i2.gamebiz.jp/images/original/1053546925ab768029bbb90011.jpg")
+      div(class="detail")
+        div(class="nav")
+          div(class="nav-button")
+            a(href="#") とくちょう
+          div(class="nav-button")
+            a(href="#") げんご
+          div(class="nav-button")
+            a(href="#") つーる
+        div(class="section feature")
+          div(class="row") しごと：がくせい・えんじにあ
+          div(class="row") せいべつ：おとこ
+          div(class="row") ばしょ：とうきょう
+
 </template>
+
+<style lang="stylus">
+.profile
+  display flex
+
+img
+  width 50%
+  height 50%
+
+.detail
+  margin 0 auto
+
+.nav
+  display flex
+
+.nav-button
+  margin 0 5px
+</style>
