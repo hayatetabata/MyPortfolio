a8b1ddebf2710665fae56bffd402a77d07d3e6e1
田畑颯 <chinarra.tabata@gmail.com>
2018-10-27 23:09:00 +0900

    Rewrite by pug and stylus

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,9 @@
-<template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        my-portfolio
-      </h1>
-      <h2 class="subtitle">
-        Home page for my portfolio
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+<template lang="pug">
+  section(class="container")
+    div
+      h1(class="title") My Portfolio Site
+      app-logo
+      h2(class="subtitle") Tap To Start
 </template>
 
 <script>
@@ -32,34 +16,33 @@ export default {
 }
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="stylus">
+.container
+  min-height 100vh
+  display flex
+  justify-content center
+  align-items center
+  text-align center
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+.title
+  font-family rpg-font-bold-12
+  display block
+  font-weight 300
+  font-size 100px
+  color #35495e
+  letter-spacing 1px
 
-.links {
-  padding-top: 15px;
-}
+.subtitle
+  font-family rpg-font-bold-12
+  font-weight 300
+  font-size 42px
+  color #526488
+  word-spacing 5px
+  padding-bottom 15px
+
+.links
+  padding-top 15px
+
 </style>
 
