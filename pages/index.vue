bc0407d681dd07cacd0db36b8f36a17ab7951e25
田畑颯 <chinarra.tabata@gmail.com>
2018-11-02 22:00:21 +0900

    Delete index

--- pages/index.vue
+++ pages/index.vue
@@ -17,32 +17,5 @@ export default {
 </script>
 
 <style lang="stylus">
-.container
-  min-height 100vh
-  display flex
-  justify-content center
-  align-items center
-  text-align center
-
-
-.title
-  font-family rpg-font-bold-12
-  display block
-  font-weight 300
-  font-size 100px
-  color white
-  letter-spacing 1px
-
-.subtitle
-  font-family rpg-font-bold-12
-  font-weight 300
-  font-size 42px
-  color white
-  word-spacing 5px
-  padding-bottom 15px
-
-.links
-  padding-top 15px
-
 </style>
 
