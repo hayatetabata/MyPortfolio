dce9eea15cd80a32c524b2503c7087722418ba78
田畑颯 <chinarra.tabata@gmail.com>
2018-11-02 23:59:50 +0900

    Fix index view

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,30 @@
 <template lang="pug">
-  section(class="container")
-    div
-      h1(class="title") My Portfolio Site
-      app-logo
-      h2(class="subtitle") Tap To Start
-      v-btn(color="primary") Normal
+  v-app
+    v-navigation-drawer(fixed app clipped)
+      v-divider
+      v-list(dense class="pt-0")
+        v-list-tile
+          v-list-tile-title Profile
+        v-divider
+        v-list-tile(@click="")
+          v-list-tile-action
+            v-icon home
+          v-list-tile-content
+            v-list-tile-title home
+        v-list-tile(@click="")
+          v-list-tile-action
+            v-icon build
+          v-list-tile-content
+            v-list-tile-title tools
+    v-toolbar(class="primary" fixed app clipped-left)
+      v-list-tile
+        v-list-title MyPortfolioSite
+    v-content
+      v-container(fill-height)
+        v-layout(justify-center align-center)
+          div
+            h1(class="title") My Portfolio Site
+            app-logo
 </template>
 
 <script>
