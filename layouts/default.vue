969e82da7b8240e1c586bd7340c0d26a50e5b5bf
田畑颯 <chinarra.tabata@gmail.com>
2018-11-03 15:53:24 +0900

    Correct index page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,49 @@
 <template lang="pug">
   v-app
-    v-navigation-drawer(fixed app clipped)
-      v-divider
-      v-list(dense class="pt-0")
-        v-list-tile
-          v-list-tile-title Profile
-        v-divider
+    v-navigation-drawer(fixed app clipped v-model="drawer")
+      v-list
         v-list-tile(to="/")
           v-list-tile-action
             v-icon home
           v-list-tile-content
             v-list-tile-title Home
+      v-list
+        v-list-tile
+          v-list-tile-title Profile
+        v-divider
         v-list-tile(to="/profile/history")
           v-list-tile-action
-            v-icon home
+            v-icon history
           v-list-tile-content
             v-list-tile-title History
 
         v-list-tile
           v-list-tile-title Works
         v-divider
-        v-list-tile(to="/languages")
+        v-list-tile(to="/works/languages")
           v-list-tile-action
             v-icon settings
           v-list-tile-content
             v-list-tile-title Languages
-        v-list-tile(to="/tools")
+        v-list-tile(to="/works/tools")
           v-list-tile-action
             v-icon build
           v-list-tile-content
             v-list-tile-title Tools
+
+        v-list-tile
+          v-list-tile-title Portfolio
+        v-divider
+        v-list-tile(to="/portfolio/products")
+          v-list-tile-action
+            v-icon phonelink
+          v-list-tile-content
+            v-list-tile-title Products
+        v-list-tile(to="/portfolio/blogs")
+          v-list-tile-action
+            v-icon edit
+          v-list-tile-content
+            v-list-tile-title Blogs
 
         v-list-tile
           v-list-tile-title Others
@@ -41,10 +55,20 @@
             v-list-tile-title Contact
     v-system-bar(fixed app color="secondary")
     v-toolbar(class="primary" fixed app clipped-left)
-      v-toolbar-side-icon(dark)
+      v-toolbar-side-icon(dark @click.stop="drawer = !drawer")
       v-list-tile
-        v-list-tile-title MyPortfolioSite
+        v-list-tile-title(class="font-weight-bold") MySite
     v-btn(fixed fab bottom right color="accent")
       v-icon add
     nuxt
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: null
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,85 @@
 <template lang="pug">
   v-content
-    v-container(fill-height)
-      v-layout(justify-center align-center)
-        div
-          h1(class="title") My Portfolio Site
-          app-logo
+    v-container(grid-list-xl)
+      v-layout
+        v-flex(xs6)
+          div(class="display-2") Profile
+      v-divider
+      v-layout
+        v-flex(xs4)
+          v-card(to="/profile/history" hover)
+            v-img(
+              class="white--text"
+              src="http://westwoodcivicclub.com/wp-content/uploads/2015/11/history.jpg"
+              :aspect-ratio="16/9"
+            )
+            v-card-title(class="headline font-weight-medium") History
+            v-card-text(class="subheading font-weight-regular") About my history before now.
+
+      v-layout
+        v-flex(xs6)
+          div(class="display-2") Works
+      v-divider
+      v-layout
+        v-flex(xs4)
+          v-card(to="/languages" hover)
+            v-img(
+              class="white--text"
+              src="https://cdn-images-1.medium.com/max/1600/1*CzwDS_yByKtMNdScO3RaPA.jpeg"
+              :aspect-ratio="16/9"
+            )
+            v-card-title(class="headline") Languages
+            v-card-text About programming languages I usually use.
+
+        v-flex(xs4)
+          v-card(to="/tools" hover)
+            v-img(
+              class="white--text"
+              src="http://cybergrota.com.pl/wp-content/uploads/2018/05/Tools-from-nathan-laytka.jpg"
+              :aspect-ratio="16/9"
+            )
+            v-card-title(class="headline") Tools
+            v-card-text About tools familier to me.
+
+      v-layout
+        v-flex(xs6)
+          div(class="display-2") Portfolio
+      v-divider
+      v-layout
+        v-flex(xs4)
+          v-card
+            v-img(
+              class="white--text"
+              src="http://atlastoolandmachine.com/wp-content/uploads/2018/03/hd-machine-wallpaper_101649396_287.jpg"
+              :aspect-ratio="16/9"
+            )
+            v-card-title(class="headline") Products
+            v-card-text Coming Soon...
+
+        v-flex(xs4)
+          v-card
+            v-img(
+              class="white--text"
+              src="https://cdn-images-1.medium.com/max/1200/1*9B7S9mipwLURUX9wdZN7Pw.jpeg"
+              :aspect-ratio="16/9"
+            )
+            v-card-title(class="headline") Blogs
+            v-card-text Coming Soon...
+
+      v-layout
+        v-flex(xs6)
+          div(class="display-2") Others
+      v-divider
+      v-layout
+        v-flex(xs4)
+          v-card
+            v-img(
+              class="white--text"
+              src="https://1.bp.blogspot.com/-D5XFUHrcow8/V5jMdu7qGPI/AAAAAAAADCE/Wzwp7tdhwoI4lYpVn3AVKAhKf89RDqEIwCLcB/s1600/writing_letter.jpg"
+              :aspect-ratio="16/9"
+            )
+            v-card-title(class="headline") Contact
+            v-card-text Coming Soon...
 </template>
 
 <script>
