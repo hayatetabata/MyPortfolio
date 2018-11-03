1ffd6c641cf88500643bab75af076cfa39903a59
田畑颯 <chinarra.tabata@gmail.com>
2018-11-03 09:20:15 +0900

    Create layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,46 @@
 <template lang="pug">
-  div
+  v-app
+    v-navigation-drawer(fixed app clipped)
+      v-divider
+      v-list(dense class="pt-0")
+        v-list-tile
+          v-list-tile-title Profile
+        v-divider
+        v-list-tile(to="/")
+          v-list-tile-action
+            v-icon home
+          v-list-tile-content
+            v-list-tile-title Home
+
+        v-list-tile
+          v-list-tile-title Works
+        v-divider
+        v-list-tile(to="languages")
+          v-list-tile-action
+            v-icon settings
+          v-list-tile-content
+            v-list-tile-title Languages
+        v-list-tile(to="tools")
+          v-list-tile-action
+            v-icon build
+          v-list-tile-content
+            v-list-tile-title Tools
+
+        v-list-tile
+          v-list-tile-title Others
+        v-divider
+        v-list-tile(to="contact")
+          v-list-tile-action
+            v-icon email
+          v-list-tile-content
+            v-list-tile-title Contact
+    v-system-bar(fixed app color="secondary")
+    v-toolbar(class="primary" fixed app clipped-left)
+      v-toolbar-side-icon(dark)
+      v-list-tile
+        v-list-tile-title MyPortfolioSite
+    v-content
+      v-btn(fixed fab bottom right color="accent")
+        v-icon add
     nuxt
 </template>
-
-<style lang="stylus">
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,9 @@
 <template lang="pug">
-  v-app
-    v-navigation-drawer(fixed app clipped)
-      v-divider
-      v-list(dense class="pt-0")
-        v-list-tile
-          v-list-tile-title Profile
-        v-divider
-        v-list-tile(@click="")
-          v-list-tile-action
-            v-icon home
-          v-list-tile-content
-            v-list-tile-title Home
-
-        v-list-tile
-          v-list-tile-title Works
-        v-divider
-        v-list-tile(@click="")
-          v-list-tile-action
-            v-icon settings
-          v-list-tile-content
-            v-list-tile-title Languages
-        v-list-tile(@click="")
-          v-list-tile-action
-            v-icon build
-          v-list-tile-content
-            v-list-tile-title Tools
-
-        v-list-tile
-          v-list-tile-title Others
-        v-divider
-        v-list-tile(@click="")
-          v-list-tile-action
-            v-icon email
-          v-list-tile-content
-            v-list-tile-title Contact
-    v-system-bar(fixed app color="secondary")
-    v-toolbar(class="primary" fixed app clipped-left)
-      v-toolbar-side-icon(dark)
-      v-list-tile
-        v-list-tile-title MyPortfolioSite
-    v-content
-      v-btn(fixed fab bottom right color="accent")
-        v-icon add
-      v-container(fill-height)
-        v-layout(justify-center align-center)
-          div
-            h1(class="title") My Portfolio Site
-            app-logo
+  v-container(fill-height)
+    v-layout(justify-center align-center)
+      div
+        h1(class="title") My Portfolio Site
+        app-logo
 </template>
 
 <script>
@@ -58,8 +15,3 @@ export default {
   }
 }
 </script>
-
-<style lang="stylus">
-
-</style>
-
