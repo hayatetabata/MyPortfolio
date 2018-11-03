93a1fbf073cc5a8d6fa141ab623c6a5fe1ce5898
田畑颯 <chinarra.tabata@gmail.com>
2018-11-03 09:49:37 +0900

    Create history template

---
+++ pages/profile/history.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  v-content
+    v-container
+      v-timeline
+        v-timeline-item(color="red lighten")
+          span(slot="opposite") High School
+          v-card(class="elevation-2")
+            v-card-title(class="headline") My High School Memories...
+            v-card-text High School
+
+        v-timeline-item(color="red lighten")
+          span(slot="opposite") University
+          v-card(class="elevation-2")
+            v-card-title(class="headline") University
+            v-card-text University
+</template>
